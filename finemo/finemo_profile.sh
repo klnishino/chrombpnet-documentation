a097656f38b81099455a4a55681f23c277310a7c
kirsten <113249781+klnishino@users.noreply.github.com>
2025-06-30 15:17:55 -0400

    Add files via upload

---
+++ finemo/finemo_profile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+#SBATCH --job-name=finemo_profile
+#SBATCH --time 14-
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --mem=96g
+#SBATCH --account=tovar0
+#SBATCH --mail-type ALL
+#SBATCH --mail-user knishino@umich.edu
+#SBATCH --output log.out
+#SBATCH --error log.err
+
+conda activate chrombpnet
+module load cuda/11.8
+module load cudnn/11.8-v8.7.0
+
+# hit calling
+finemo call-hits -r /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile.npz \
+	-m  /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/modisco_w_500/modisco.h5 \
+	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile \
+	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
+	-J
+
+	
+
