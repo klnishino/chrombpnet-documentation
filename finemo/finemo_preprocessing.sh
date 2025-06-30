09039536ecaaa3159eb95f2ee48a61bc3d41dcf8
kirsten <113249781+klnishino@users.noreply.github.com>
2025-06-30 15:17:32 -0400

    Create finemo_preprocessing.sh

---
+++ finemo/finemo_preprocessing.sh
@@ -0,0 +1,12 @@
+# preprocessing (doesn't require gpu)
+conda activate chrombpnet
+
+finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_0/contribs_bw/fold_0_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_1/contribs_bw/fold_1_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_2/contribs_bw/fold_2_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_3/contribs_bw/fold_3_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_4/contribs_bw/fold_4_contribs_bw.profile_scores.h5 \
+	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile \
+	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
+	-w 500
+
+finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_0/contribs_bw/fold_0_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_1/contribs_bw/fold_1_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_2/contribs_bw/fold_2_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_3/contribs_bw/fold_3_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_4/contribs_bw/fold_4_contribs_bw.counts_scores.h5 \
+	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/counts \
+	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
+	-w 500
