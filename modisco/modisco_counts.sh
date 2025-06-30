3a9af1aa9037b3a958eedb98104bbdec4102a7a1
kirsten <113249781+klnishino@users.noreply.github.com>
2025-06-30 14:20:00 -0400

    Add files via upload

---
+++ modisco/modisco_counts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=modisco_counts
+#SBATCH --time 14-
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem 180g
+#SBATCH --account=tovar0
+#SBATCH --mail-type ALL
+#SBATCH --mail-user knishino@umich.edu
+#SBATCH --output log.out
+#SBATCH --error log.err
+
+conda activate chrombpnet
+
+modisco motifs -i /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_counts_folds/mean_contribs_bw.counts_scores.h5 \
+-n 1000000 \
+-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_counts_folds/modisco/modisco_results.h5 \
+-v
+
---
+++ modisco/modisco_profie_report.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=modisco_profile_report
+#SBATCH --time 14-
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem 32g
+#SBATCH --account=tovar0
+#SBATCH --mail-type ALL
+#SBATCH --mail-user knishino@umich.edu
+#SBATCH --output log.out
+#SBATCH --error log.err
+
+# do once for download motifs of choice
+# wget \
+#	https://jaspar.genereg.net/download/data/2022/CORE/JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt \
+#	-O /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
+
+conda activate chrombpnet
+
+modisco report -i  /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/modisco_w_500/modisco_results.h5 \
+	-o report/ \
+	-s /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/modisco_w_500/report/ \
+	-m /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/JASPAR2022_CORE_vertebrates_non-redundant_pfms_meme.txt
