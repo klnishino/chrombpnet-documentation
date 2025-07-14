7b787ebf436d169e4767ee54c4f7412701084016
kirsten <113249781+klnishino@users.noreply.github.com>
2025-07-14 13:20:30 -0400

    Update finemo_preprocessing.sh

--- finemo/finemo_preprocessing.sh
+++ finemo/finemo_preprocessing.sh
@@ -1,5 +1,5 @@
 # preprocessing (doesn't require gpu)
-conda activate chrombpnet
+conda activate finemo
 
 finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_0/contribs_bw/fold_0_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_1/contribs_bw/fold_1_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_2/contribs_bw/fold_2_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_3/contribs_bw/fold_3_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_4/contribs_bw/fold_4_contribs_bw.profile_scores.h5 \
 	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile \
