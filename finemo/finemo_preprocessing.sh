72a237d50063d4fc0695963252a1b169b8d18770
kirsten <113249781+klnishino@users.noreply.github.com>
2025-07-14 13:26:04 -0400

    Update finemo_preprocessing.sh

--- finemo/finemo_preprocessing.sh
+++ finemo/finemo_preprocessing.sh
@@ -1,12 +1,9 @@
 # preprocessing (doesn't require gpu)
 conda activate finemo
 
-finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_0/contribs_bw/fold_0_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_1/contribs_bw/fold_1_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_2/contribs_bw/fold_2_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_3/contribs_bw/fold_3_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_4/contribs_bw/fold_4_contribs_bw.profile_scores.h5 \
-	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile \
+finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/fold_0/fold_0_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/fold_1/fold_1_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/fold_2/fold_2_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/fold_3/fold_3_contribs_bw.profile_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/fold_4/fold_4_contribs_bw.profile_scores.h5 \
+	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/finemo/profile \
 	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
 	-w 500
 
-finemo extract-regions-chrombpnet-h5 -c /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_0/contribs_bw/fold_0_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_1/contribs_bw/fold_1_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_2/contribs_bw/fold_2_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_3/contribs_bw/fold_3_contribs_bw.counts_scores.h5 /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/fold_4/contribs_bw/fold_4_contribs_bw.counts_scores.h5 \
-	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/counts \
-	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
-	-w 500
+
