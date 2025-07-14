b3a9da26616451bd8e0faa0122a5a83ed4adddf6
kirsten <113249781+klnishino@users.noreply.github.com>
2025-07-14 13:18:14 -0400

    Update finemo_profile.sh

--- finemo/finemo_profile.sh
+++ finemo/finemo_profile.sh
@@ -11,16 +11,13 @@
 #SBATCH --output log.out
 #SBATCH --error log.err
 
-conda activate chrombpnet
+conda activate finemo
 module load cuda/11.8
 module load cudnn/11.8-v8.7.0
 
 # hit calling
-finemo call-hits -r /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile.npz \
-	-m  /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/modisco_w_500/modisco.h5 \
-	-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/finemo/profile \
-	-p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
-	-J
-
-	
-
+finemo call-hits -r /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/finemo/profile.npz \
+        -m /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/modisco-mt/modisco.h5 \
+        -o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/finemo/profile \
+        -p /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/data/downloads/merged_windows.bed \
+        -J
