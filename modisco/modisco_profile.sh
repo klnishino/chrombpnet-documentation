005fd2368e86d3715c42cd281360056ac4ed3817
kirsten <113249781+klnishino@users.noreply.github.com>
2025-07-14 13:11:11 -0400

    Update modisco_profile.sh

--- modisco/modisco_profile.sh
+++ modisco/modisco_profile.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=modisco
 #SBATCH --time 14-
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem 180g
+#SBATCH --cpus-per-task=24
+#SBATCH --mem 32g
 #SBATCH --account=tovar0
 #SBATCH --mail-type ALL
 #SBATCH --mail-user knishino@umich.edu
@@ -13,9 +13,11 @@
 
 conda activate chrombpnet
 
-modisco motifs -i /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/mean_contribs_bw.profile_scores.h5 \
+export NUMBA_NUM_THREADS=24
+
+modisco motifs -i /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/mean_contribs_bw.profile_scores.h5 \
 -n 1000000 \
--o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/merged_profile_folds/modisco/modisco_results.h5 \
+-o /scratch/tovar_root/tovar0/knishino/chrombpnet-encode-sm/updated_contribs/profile_only/modisco-mt/modisco_results.h5  \
 -w 500 \
 -v
 
